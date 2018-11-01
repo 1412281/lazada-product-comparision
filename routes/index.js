2067cae072676265d80ae3a5459940a574de3b34
Trần Thanh Lâm <lamtt@tictag.vn>
2018-11-01 17:20:26 +0700

    get general data from source -> client req ajax

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const lazada_product = require('../models/lazada-product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/product', function (req, res) {
+    const productURL = req.body.productURL;
+    lazada_product.getProductLazada(productURL).then((product) => {
+        res.json(product);
+    }).catch((err) => {
+        res.status(200).end(err);
+    });
+
+});
 module.exports = router;
